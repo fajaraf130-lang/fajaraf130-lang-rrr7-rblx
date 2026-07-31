9fb2fe3ae236b5a422827868e195fd19184f2978
fajaraf130-lang <fajaraf130@gmail.com>
2026-07-31 07:12:01 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+
+apiKey: "ISI_API_KEY",
+authDomain: "ISI_DOMAIN",
+databaseURL: "ISI_DATABASE_URL",
+projectId: "ISI_PROJECT_ID"
+
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+
+const db =
+firebase.database();
